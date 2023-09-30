f40cdfa430878c2872d1422aef574634d846fd9c
hassansadeedali <hassan.sadeedali@gmail.com>
2023-09-30 20:02:13 +0530

    [Binary Tree]

---
+++ Blind75/Trees/BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Blind75.Trees
+{
+    public class BinaryTreeLevelOrderTraversal
+    {
+        //Iterative - uses BFS
+        public static TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        public static IList<IList<int>> LevelOrder()
+        {
+            var result = new List<IList<int>>();
+
+            if (root == null)
+                return result;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (true)
+            {
+                var curLevel = q.Count;
+                if (curLevel == 0)
+                    break;
+                var curNodes = new List<int>();
+
+                while (curLevel > 0)
+                {
+                    var cur = q.Dequeue();
+                    curNodes.Add(cur.val);
+
+                    if (cur.left != null)
+                        q.Enqueue(cur.left);
+
+                    if (cur.right != null)
+                        q.Enqueue(cur.right);
+
+                    curLevel--;
+                }
+
+                result.Add(curNodes);
+            }
+
+            return result;
+        }
+    }
+}
+
+
+
+
---
+++ Blind75/Trees/KthSmallestElementInABST.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Blind75.Trees
+{
+    class KthSmallestElementinaBST
+    {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        public int KthSmallest(TreeNode root, int k)
+        {
+            TraverseToEnd(root);
+
+            TreeNode node = null; 
+            while (k > 0)
+            {
+                //If k=1, the very first element is the answer, since this is a BST
+                node = stack.Pop();
+                k--;
+
+                if (k == 0)
+                    break;
+
+                TraverseToEnd(node.right);
+            }
+
+            return node.val;
+        }
+
+        private void TraverseToEnd(TreeNode root)
+        {
+            while (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+        }
+    }
+}
