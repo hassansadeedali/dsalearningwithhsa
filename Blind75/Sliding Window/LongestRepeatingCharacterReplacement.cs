cfd1cb3c6de6619cfe0a83fa39945a6842dac583
hassansadeedali <hassan.sadeedali@gmail.com>
2023-09-30 13:37:44 +0530

    [Sliding window]

---
+++ Blind75/Sliding Window/LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Blind75.Sliding_Window
+{
+    /*
+    You are given a string s and an integer k. 
+    You can choose any character of the string 
+    and change it to any other uppercase English character. 
+    You can perform this operation at most k times.
+    
+    Return the length of the longest substring containing 
+    the same letter you can get after performing the above operations.
+    */
+    public class LongestRepeatingCharacterReplacement
+    {
+        public static string s = "AABABBA";
+        public static int k = 1;
+
+        //Input: s = "AABABBA", k = 1
+        //Output: 4
+        //Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+        //The substring "BBBB" has the longest repeating letters, which is 4.
+        //There may exists other ways to achive this answer too.
+
+        public static int Handle()
+        {
+            int left = 0;
+            int maxLength = 0;
+            int mostFrequentLetterCount = 0; // Count of most frequent letter in the window
+            int[] charCounts = new int[26]; // Counts per letter
+
+            for(int right=0; right < s.Length; right++)
+            {
+                charCounts[s[right] - 'A']++;
+                mostFrequentLetterCount = Math.Max(mostFrequentLetterCount, charCounts[s[right] - 'A']);
+
+                int lettersToChange = (right - left + 1) - mostFrequentLetterCount;
+
+                //Out of the window
+                if (lettersToChange > k)
+                {
+                    charCounts[s[left] - 'A']--;
+                    left++;
+                }
+
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ Blind75/Sliding Window/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blind75.Sliding_Window
+{
+    /*
+    Given a string s, find the length of the longest substring
+    without repeating characters.
+    */
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public static string s = "abcabcbb";
+        //Input: s = "abcabcbb"
+        //Output: 3
+        //Explanation: The answer is "abc", with the length of 3.
+
+        public static int Handle()
+        {
+            int left = 0;
+            int right = 0;
+            int max = 0;
+            HashSet<int> chars = new();
+
+            //Iterate until right has reached the length
+            while (right < s.Length)
+            {
+                var currChar = s[right];
+                if (chars.Contains(currChar))
+                {
+                    //If Hashset has the character, then remove it from hashet and move the left pointer
+                    chars.Remove(s[left]);
+                    left++;
+                }
+                else
+                {
+                    //If Hashset doesnt have the character, then add to hashet and move the right pointer
+                    chars.Add(currChar);
+                    max = Math.Max(max, right - left + 1);
+                    right++;
+                }
+
+            }
+
+            return max;
+        }
+    }
+}
